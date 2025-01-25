a65f6e0997254e5c762675f2c584b775247d56c9
Jatan Dudeja <dudeja.jatan04@gmail.com>
2025-01-25 20:01:29 +0530

    feature: added get orderdetails by tourID, fixed some issues.

--- src/controllers/purchasedTour.controller.ts
+++ src/controllers/purchasedTour.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import PurchasedTour from "../models/purchasedTour.model.js";
+import { GlobalRequestDTO } from "../types/user.types.js";
 
 export class PurchasedTourController {
   async createPurchasedTour(req: Request, res: Response) {
@@ -22,6 +23,26 @@ export class PurchasedTourController {
       res
         .status(200)
         .json({ statusCode: 200, message: "Purchase added successfully!" });
+    } catch (error) {
+      res.status(500).json({
+        statusCode: 500,
+        message: "Please try again after sometime",
+      });
+    }
+    return;
+  }
+
+  async getPurchasedToursByUserID(req: Request, res: Response) {
+    const { userID } = (req as GlobalRequestDTO);
+    const tourID = req.params.tourID;
+
+    try {
+      const purchasedTours = await PurchasedTour.find({ userID, tourID });
+
+      res.status(200).json({
+        statusCode: 200,
+        purchasedTours,
+      });
     } catch (error) {
       res.status(500).json({
         statusCode: 500,
--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -16,7 +16,7 @@ const orderSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    status: {
+    status: { // 0 -> Pending, 1 -> Success, 2 -> Failed
       type: Number,
       enum: [0, 1, 2],
       required: true,
