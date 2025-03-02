58879e7e1fe871b895d65da7ac8717e1a3181ef0
Jatan Dudeja <dudeja.jatan04@gmail.com>
2025-03-02 18:06:37 +0530

    dockerized this repo.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt, { SignOptions } from "jsonwebtoken";
 import { UserDTO } from "../types/user.types.js";
 import { APIErrors } from "../utils/apiErrors.js";
